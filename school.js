f95fc6f5533a86ebdbfce5ab75fee53f3429996a
Raphael Gobatto Laranjeira <raphagobatto@hotmail.com>
2022-07-25 14:55:09 -0300

    Update school.js
    setting high school

--- school.js
+++ school.js
@@ -22,16 +22,45 @@ class School {
     }
     quickFacts() {
       console.log(
-        `${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`);
+        `${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`
+      );
     }
     static pickSubstituteTeacher(substituteTeachers) {
-      const randIt = MAth.floor(Math.random() substituteTeachers.length);
-      return substituteTeachers[randIt];
+      const randInt = Math.floor(Math.random() * substituteTeachers.length);
+      return substituteTeachers[randInt];
     }
-}
+  }
   
-class PrimarySchool extends School {
-    constructor(name, numberOfStudents, pickupPolicy)
-    super(name, 'primary', numberOfStudents);
-    this.pickupPolicy = pickupPolicy
-}
+  class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickupPolicy) {
+      super(name, "primary", numberOfStudents);
+      this._pickupPolicy = pickupPolicy;
+    }
+  
+    get pickupPolicy() {
+      return this._pickupPolicy;
+    }
+  }
+  
+  class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+      super(name, "high", numberOfStudents);
+      this._sportsTeams = sportsTeams;
+    }
+  
+    get sportsTeams() {
+      return this._sportsTeams;
+    }
+  }
+  
+  const lorraineHansbury = new PrimarySchool(
+    "Lorraine Hansbury",
+    514,
+    "Students must be picked up by a parent, guardian, or a family member over the age of 13."
+  );
+  
+  lorraineHansbury.quickFacts();
+  const sub = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+  
+  const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field'])
+  
